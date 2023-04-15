4fc2b249a76d0a5748e764371faf5e76898f4bb8
jlscripts <120191361+jlscripts@users.noreply.github.com>
2023-04-15 23:34:44 +0800

    Fetch data using firebase

--- src/components/Products/AvailableProducts.js
+++ src/components/Products/AvailableProducts.js
@@ -1,66 +1,63 @@
+import React, { useEffect, useState } from "react";
+import Loading from "../UI/Loading";
 import classes from "./AvailableProducts.module.css";
 import ProductItem from "./ProductItem";
-import espresso from "../../assets/espresso.jpg";
-
-const DUMMY_PRODUCTS = [
-  {
-    id: "c1",
-    name: "Espresso",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus magna nec lectus bibendum, eu convallis nibh dignissim.",
-    price: 120,
-    imgsrc: { espresso },
-  },
-  {
-    id: "c2",
-    name: "Americano",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus magna nec lectus bibendum, eu convallis nibh dignissim.",
-    price: 80,
-  },
-  {
-    id: "c3",
-    name: "Vanilla Latte",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus magna nec lectus bibendum, eu convallis nibh dignissim.",
-    price: 140,
-  },
-  {
-    id: "c4",
-    name: "Machiato",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus magna nec lectus bibendum, eu convallis nibh dignissim.",
-    price: 120,
-  },
-  {
-    id: "c5",
-    name: "Mocha",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus magna nec lectus bibendum, eu convallis nibh dignissim.",
-    price: 140,
-  },
-  {
-    id: "c6",
-    name: "Capuccino",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed finibus magna nec lectus bibendum, eu convallis nibh dignissim.",
-    price: 120,
-  },
-];
 
 const AvailableProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch(
+        "https://brewskobros-default-rtdb.asia-southeast1.firebasedatabase.app/coffees.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const productsData = await response.json();
+      const loadedProducts = [];
+
+      for (const key in productsData) {
+        loadedProducts.push({
+          id: key,
+          name: productsData[key].name,
+          description: productsData[key].description,
+          price: productsData[key].price,
+        });
+      }
+
+      setProducts(loadedProducts);
+      setIsLoading(false);
+    };
+
+    fetchProducts()
+      .then()
+      .catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+      });
+  }, []);
+
   return (
-    <ul className={classes.menuItems}>
-      {DUMMY_PRODUCTS.map((product) => (
-        <ProductItem
-          key={product.id}
-          id={product.id}
-          name={product.name}
-          description={product.description}
-          price={product.price}
-        ></ProductItem>
-      ))}
-    </ul>
+    <React.Fragment>
+      {isLoading && <Loading></Loading>}
+      {httpError && <p className={classes.errorContent}>{httpError}</p>}
+      <ul className={classes.menuItems}>
+        {products.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            description={product.description}
+            price={product.price}
+          ></ProductItem>
+        ))}
+      </ul>
+    </React.Fragment>
   );
 };
 
